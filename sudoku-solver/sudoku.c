d6333dc05976df6e426ffd26c839e3e4782a531b
Pratik12350 <ss2983488@gmail.com>
2025-01-08 18:33:25 +0530

    Fixed typo and formatted

--- sudoku-solver/sudoku.c
+++ sudoku-solver/sudoku.c
@@ -24,7 +24,8 @@ int main(int argc, char *argv[]) {
                     {8, 0, 0, 0, 3, 0, 0, 0, 0},
                     {9, 0, 2, 5, 1, 0, 0, 7, 6}};
 
-  if(solveGrid(grid)) {
+  // clang-format on
+  if (solveGrid(grid)) {
     printGrid(grid);
   } else {
     printf("Failed to solve sudoku.\n");
@@ -32,17 +33,16 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-
-int solveGrid(int grid[N][N])
-{
+int solveGrid(int grid[N][N]) {
   int r, c;
-  if(findUnassigned(grid, &r, &c)) {
-    for(int n = 1; n <= 9; n++) {
-      if(isValid(grid, n, r, c)) {
+  if (findUnassigned(grid, &r, &c)) {
+    for (int n = 1; n <= 9; n++) {
+      if (isValid(grid, n, r, c)) {
         grid[r][c] = n;
 
-        // i wish her love for me had recurssion
-        if(solveGrid(grid)) return 1;
+        // i wish her love for me had rrecursion
+        if (solveGrid(grid))
+          return 1;
         grid[r][c] = UNASSIGNED;
       }
     }
@@ -53,9 +53,7 @@ int solveGrid(int grid[N][N])
   return 0;
 }
 
-
-void printGrid(int grid[N][N])
-{
+void printGrid(int grid[N][N]) {
   for (int r = 0; r < N; r++) {
     for (int c = 0; c < N; c++) {
       printf(" %d ", grid[r][c]);
@@ -64,11 +62,10 @@ void printGrid(int grid[N][N])
   }
 }
 
-int findUnassigned(int grid[N][N], int *r, int *c)
-{
-  for(*r = 0; *r < N; (*r)++) {
-    for(*c = 0; *c < N; (*c)++) {
-      if(grid[*r][*c] == UNASSIGNED) {
+int findUnassigned(int grid[N][N], int *r, int *c) {
+  for (*r = 0; *r < N; (*r)++) {
+    for (*c = 0; *c < N; (*c)++) {
+      if (grid[*r][*c] == UNASSIGNED) {
         return 1;
       }
     }
@@ -76,32 +73,28 @@ int findUnassigned(int grid[N][N], int *r, int *c)
   return 0;
 }
 
-
-int isValidInRow(int grid[N][N], int number, int r)
-{
-  for(int c = 0; c < N; c++) {
-    if(grid[r][c] == number) {
+int isValidInRow(int grid[N][N], int number, int r) {
+  for (int c = 0; c < N; c++) {
+    if (grid[r][c] == number) {
       return 0;
     }
   }
   return 1;
 }
 
-int isValidInColumn(int grid[N][N], int number, int c)
-{
-  for(int r = 0; r < N; r++) {
-    if(grid[r][c] == number) {
+int isValidInColumn(int grid[N][N], int number, int c) {
+  for (int r = 0; r < N; r++) {
+    if (grid[r][c] == number) {
       return 0;
     }
   }
   return 1;
 }
 
-int isValidInBox(int grid[N][N], int number, int rowStart, int colStart)
-{
-  for(int r = 0; r < 3; r++) {
-    for(int c = 0; c < 3; c++) {
-      if(grid[rowStart + r][colStart + c] == number) {
+int isValidInBox(int grid[N][N], int number, int rowStart, int colStart) {
+  for (int r = 0; r < 3; r++) {
+    for (int c = 0; c < 3; c++) {
+      if (grid[rowStart + r][colStart + c] == number) {
         return 0;
       }
     }
@@ -109,8 +102,10 @@ int isValidInBox(int grid[N][N], int number, int rowStart, int colStart)
   return 1;
 }
 
-
 int isValid(int grid[N][N], int number, int r, int c) {
-  if(grid[r][c] == UNASSIGNED && isValidInRow(grid, number, r) && isValidInColumn(grid, number, c) && isValidInBox(grid, number, r - r % 3, c - c % 3)) return 1;
+  if (grid[r][c] == UNASSIGNED && isValidInRow(grid, number, r) &&
+      isValidInColumn(grid, number, c) &&
+      isValidInBox(grid, number, r - r % 3, c - c % 3))
+    return 1;
   return 0;
 }
